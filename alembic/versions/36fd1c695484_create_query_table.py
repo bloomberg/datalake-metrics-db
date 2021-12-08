f17d89c7c9d1ce1085aa0e02af1d826625169727
Cristian Osiac <mosiac@bloomberg.net>
2021-12-08 11:17:56 +0000

    Combine alembic and license header

--- alembic/versions/36fd1c695484_create_query_table.py
+++ alembic/versions/36fd1c695484_create_query_table.py
@@ -12,10 +12,9 @@
  ** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  ** See the License for the specific language governing permissions and
  ** limitations under the License.
-"""
 
 
-"""create table query_metrics
+create table query_metrics
 
 Revision ID: 36fd1c695484
 Revises:
